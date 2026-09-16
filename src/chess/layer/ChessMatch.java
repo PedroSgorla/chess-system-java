4b44f2c8c672466533e98ce3118e281a6476c596
Pedro Sgorla <pedro.oliveira@atlantecnologia.com.br>
2026-09-16 16:04:29 -0300

    Piece move count

--- src/chess/layer/ChessMatch.java
+++ src/chess/layer/ChessMatch.java
@@ -85,9 +85,10 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece pieceSource = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
-		board.placePiece(pieceSource, target);
+		board.placePiece(p, target);
 		
 		if(capturedPiece != null) {
 			piecesOnTheBoard.remove(capturedPiece);
@@ -98,7 +99,8 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.deincreaseMoveCount();
 		board.placePiece(p, source);
 		
 		if(capturedPiece != null) {
@@ -107,6 +109,7 @@ public class ChessMatch {
 			piecesOnTheBoard.add(capturedPiece);
 		}
 	}
+	
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
--- src/chess/layer/ChessPiece.java
+++ src/chess/layer/ChessPiece.java
@@ -7,15 +7,28 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
 	
 	private Color color;
-
+	private int moveCount;
+	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
-
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void deincreaseMoveCount() {
+		moveCount--;
+	}
+	
 	public Color getColor() {
 		return color;
 	}	
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
